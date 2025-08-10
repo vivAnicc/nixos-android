4791ba06a71c73379384bcb2348c975683e09e49
nick <nicc.gemm@gmail.com>
2025-08-10 11:44:05 +0000

    Fix zig and zls by installing them system-wide

--- modules/zig.nix
+++ modules/zig.nix
@@ -1,8 +1,15 @@
-{ pkgs, inputs, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 {
 	home.packages = [
 		inputs.zig.packages."${pkgs.system}".master
 		inputs.zls.packages."${pkgs.system}".zls
 	];
+
+	programs.nixvim.plugins.zig = lib.mkIf config.programs.nixvim.enable {
+		enable = true;
+		settings = {
+			fmt_autosave = 0;
+		};
+	};
 }
