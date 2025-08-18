e8bff2f2d732e2a0191395f5b553a857eb41be35
nick <nicc.gemm@gmail.com>
2025-08-18 20:41:44 +0000

    Adjusted host key for ip of the phone

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -9,7 +9,7 @@
     knownHosts = {
       "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
 
-      "[10.121.67.34]:2222".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      "[10.121.67.34]:2222".publicKey = "AAAAC3NzaC1lZDI1NTE5AAAAIEn0p+TyFK0iWUGa2gR4SkwUmhsM2vbTVoB6WkRhQacf";
     };
   };
 
